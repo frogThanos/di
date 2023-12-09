f20b3bddbf8125bb0a529ad56b89b11987b135df
Thanos Kyriakakis <thanos.kyriakakis@persegroep.net>
2023-12-09 09:40:02 +0100

    connect cpu module and disk module to computer module

--- src/computer/computer.controller.ts
+++ src/computer/computer.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiskService } from 'src/disk/disk.service';
 
 @Controller('computer')
-export class ComputerController {}
+export class ComputerController {
+  constructor(
+    private cpuService: CpuService,
+    private diskService: DiskService,
+  ) {}
+
+  run() {
+    this.cpuService.compute(1, 1);
+    this.diskService.getData();
+  }
+}
